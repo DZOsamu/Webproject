5f080e639e049cb39ceb456e6ba3718c8902abbb
Osamu <1226924006@qq.com>
2023-07-20 23:22:01 +0800

    图书管理

---
+++ 前后端交互/Ajax/new/09图书管理/js/index.js
@@ -0,0 +1,27 @@
+/**
+ * 目标1：渲染图书列表
+ *  1.1 获取数据
+ *  1.2 渲染数据
+ */
+
+// 定义一个全局常量 外号
+const creator = '池舟'
+
+// 封装-获取并渲染图书列表的函数
+function getBooksList() {
+   // 1.1获取数据
+   axios({
+      url: 'http://hmajax.itheima.net/api/books',
+      params: {
+         // 外号：获取对应的数据
+         creator
+      }
+   }).then(result => {
+      // console.log(result)
+      const bookList = result.data.data
+      console.log(bookList)
+   })
+}
+
+// 网页加载运行，获取并渲染列表一次
+getBooksList()
