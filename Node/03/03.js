04b5cc134d1df30bbedc315c8671a963439979c9
Osamu <1226924006@qq.com>
2023-08-02 14:33:39 +0800

    path模块

---
+++ Node/03/03.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+
+// 1.引入path对象
+const path = require('path')
+
+// 2.调用path.join()配合__dirname组成目标文件的绝对路径
+console.log(__dirname)
+
+fs.readFile(path.join(__dirname, '../test.txt'), (err, data) => {
+   if (err) console.log(err)
+   else console.log(data.toString())
+})
