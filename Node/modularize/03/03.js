97263386f609e373d46ada309290ea6f773c468c
Osamu <1226924006@qq.com>
2023-08-06 11:06:16 +0800

    ECMAScript标准命名导入导出

---
+++ Node/modularize/03/03.js
@@ -0,0 +1,7 @@
+// 命名导入
+import { baseURL, getArraySum } from "./utils.js"
+console.log(baseURL)
+console.log(getArraySum)
+
+const result = getArraySum([20, 21, 13])
+console.log(result)
---
+++ Node/modularize/03/utils.js
@@ -0,0 +1,9 @@
+/**
+ * 目标：基于 ECMAScript标准 语法，封装属性和方法并"命名"导出
+ */
+// const baseURL = 'http://hmajax.itheima.net'
+// const getArraySum = arr => arr.reduce((sum, item) => sum += item, 0)
+
+// 命名导出
+export const baseURL = 'http://hmajax.itheima.net'
+export const getArraySum = arr => arr.reduce((sum, item) => sum += item, 0)
