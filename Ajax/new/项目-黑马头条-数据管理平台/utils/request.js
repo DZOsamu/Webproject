821b9533707865f209005121b92ae613af50664b
Osamu <1226924006@qq.com>
2023-09-09 21:15:18 +0800

    项目-axios响应拦截器和身份验证失败

--- Ajax/new/项目-黑马头条-数据管理平台/utils/request.js
+++ Ajax/new/项目-黑马头条-数据管理平台/utils/request.js
@@ -8,8 +8,26 @@ axios.interceptors.request.use(function (config) {
    // 统一携带token令牌字符串在请求头
    const token = localStorage.getItem('token')
    token && (config.headers.Authorization = `Bearer ${token}`)
-   
+
    return config
 }, function (error) {
+   return Promise.reject(error)
+})
+
+// 添加响应拦截器
+axios.interceptors.response.use(function (response){
+   // 2xx的状态码都会触发该函数
+   // 对响应数据做点什么
+   return response
+},function (error){
+   // 超出2xx范围内的状态码都会触发该函数
+   // 对响应错误做点什么
+   // 例如 统一对401身份验证失败情况做出处理
+   console.dir(error)
+   if(error?.response?.status===401){
+      alert('身份验证失败，请重新登录')
+      localStorage.clear()
+      location.href = '../login/index.html'
+   }
    return Promise.reject(error)
 })
