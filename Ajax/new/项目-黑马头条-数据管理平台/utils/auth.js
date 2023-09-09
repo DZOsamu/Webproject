02d9d4c803c2b18bb03ad439529b2e7e6954d4d9
Osamu <1226924006@qq.com>
2023-09-09 20:21:25 +0800

    项目-设置个人信息和axios请求拦截器

--- Ajax/new/项目-黑马头条-数据管理平台/utils/auth.js
+++ Ajax/new/项目-黑马头条-数据管理平台/utils/auth.js
@@ -3,21 +3,25 @@
 // 1.访问权限控制
 // 1.1 判断无token令牌字符串，则强制跳转到登录页
 const token = localStorage.getItem('token')
-if(!token) {
+if (!token) {
    location.href = '../login/index.html'
 }
-// 1.2 登录成功后，保存token令牌字符串到本地，并跳转到内容列表页面
+// 1.2 登录成功后，保存token令牌字符串到本地，并跳转到内容列表页面（publish/index.js）
+
+
+// 目标2.设置个人信息
+// 2.1 在utils/request.js设置请求拦截器，统一携带token
+// 2.2 请求个人信息并设置到页面
+axios({
+   url: '/v1_0/user/profile'
+}).then(result => {
+   console.log(result)
+   const username = result.data.data.name
+   document.querySelector('.nick-name').innerHTML = username
+})
 
 
 
-
-
-
-/**
- * 目标2：设置个人信息
- * 2.1 在 utils/request.js 设置请求拦截器，统一携带 token
- * 2.2 请求个人信息并设置到页面
- */
 
 /**
  * 目标3：退出登录
--- Ajax/new/项目-黑马头条-数据管理平台/utils/request.js
+++ Ajax/new/项目-黑马头条-数据管理平台/utils/request.js
@@ -1,3 +1,15 @@
 // axios 公共配置
 // 基地址
 axios.defaults.baseURL = 'http://geek.itheima.net'
+
+// 添加请求拦截器
+axios.interceptors.request.use(function (config) {
+   // 在发送请求之前做什么
+   // 统一携带token令牌字符串在请求头
+   const token = localStorage.getItem('token')
+   token && (config.headers.Authorization = `Bearer ${token}`)
+   
+   return config
+}, function (error) {
+   return Promise.reject(error)
+})
