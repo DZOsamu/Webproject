8ada292e79ac19388a33c01ffe65152c86e636c1
Osamu <1226924006@qq.com>
2023-09-13 21:34:38 +0800

    项目-退出登录

--- Ajax/new/项目-黑马头条-数据管理平台/utils/auth.js
+++ Ajax/new/项目-黑马头条-数据管理平台/utils/auth.js
@@ -9,7 +9,7 @@ if (!token) {
 // 1.2 登录成功后，保存token令牌字符串到本地，并跳转到内容列表页面（publish/index.js）
 
 
-// 目标2.设置个人信息
+// 2.设置个人信息
 // 2.1 在utils/request.js设置请求拦截器，统一携带token
 // 2.2 请求个人信息并设置到页面
 axios({
@@ -20,11 +20,10 @@ axios({
    document.querySelector('.nick-name').innerHTML = username
 })
 
-
-
-
-/**
- * 目标3：退出登录
- *  3.1 绑定点击事件
- *  3.2 清空本地缓存，跳转到登录页面
- */
+// 3.退出登录
+// 3.1 绑定点击事件
+document.querySelector('.quit').addEventListener('click', e => {
+   // 3.2 清空本地缓存，跳转到登录页面
+   localStorage.clear()
+   location.href = '../login/index.html'
+})
