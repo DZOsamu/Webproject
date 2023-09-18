0ae9915c02ab7c9a3e75d9649011ff0852a0934f
Osamu <1226924006@qq.com>
2023-09-18 12:33:06 +0800

    修改

--- Node/webpack/08/src/login/index.js
+++
@@ -1,8 +0,0 @@
-// 命名导入
-import { checkPhone,checkCode } from "../utils/check.js"
-console.log(checkPhone('15671275650'))
-console.log(checkCode('1234567'))
-
-// 准备webpack打包环境
-
-// 运行：npm run 自定义命令
--- Node/webpack/08/src/utils/check.js
+++
@@ -1,5 +0,0 @@
-// 封装校验手机号长度、验证码长度的函数
-export const checkPhone = phone => phone.length === 11
-export const checkCode = code => code.length === 6
-
-// 命名导出 加export
