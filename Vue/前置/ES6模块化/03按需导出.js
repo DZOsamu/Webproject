91753658f2ff24ba1ad822d556ec658f46da155a
Osamu <1226924006@qq.com>
2023-10-09 19:30:53 +0800

    注意

--- Vue/前置/ES6模块化/03按需导出.js
+++ Vue/前置/ES6模块化/03按需导出.js
@@ -1,4 +1,8 @@
 export let s1 = 'aaa'
 export let s2 = 'ccc'
 
-export function say() {}
+export function say() { }
+
+export default {
+   a: 20
+}
--- Vue/前置/ES6模块化/04按需导入.js
+++ Vue/前置/ES6模块化/04按需导入.js
@@ -1,5 +1,8 @@
-import { s1, s2, say } from './03按需导出.js'
+import info, { s1, s2 as str2, say } from './03按需导出.js'
 
 console.log(s1)
-console.log(s2)
+// console.log(s2)    // 报错
+console.log(str2)
 console.log(say)
+
+console.log(info)   // 指向默认导出的对象
