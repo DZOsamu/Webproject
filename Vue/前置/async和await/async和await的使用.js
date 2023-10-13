839920296c06e6381a4009ddd3e6183fde4321f8
Osamu <1226924006@qq.com>
2023-10-13 22:53:03 +0800

    async/await注意

--- Vue/前置/async和await/async和await的使用.js
+++ Vue/前置/async和await/async和await的使用.js
@@ -1,12 +1,20 @@
 import thenFs from 'then-fs'
 
+console.log('A')
+
 async function getAllFile() {
+
+   console.log('B')
+
    const r1 = await thenFs.readFile('../Promise/files/1.txt', 'utf8')
-   console.log(r1)
    const r2 = await thenFs.readFile('../Promise/files/2.txt', 'utf8')
-   console.log(r2)
    const r3 = await thenFs.readFile('../Promise/files/3.txt', 'utf8')
-   console.log(r3)
+   console.log(r1, r2, r3)
+
+   console.log('D')
+
 }
 
 getAllFile()
+
+console.log('C')
