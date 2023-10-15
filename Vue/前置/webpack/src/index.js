f8a15c3f355599b11fb464a9b4354ae023c39ec0
Osamu <1226924006@qq.com>
2023-10-15 21:29:24 +0800

    webpack再次打包

--- Vue/前置/webpack/src/index.js
+++ Vue/前置/webpack/src/index.js
@@ -1,3 +1,6 @@
 // webpack打包的入口
-import { addFn } from "./add/add.js"
+import { addFn } from "./add/add"
+import { getArrSum } from "./tool/tool"
+
 console.log(addFn(2, 5))
+console.log(getArrSum([2, 4, 5, 7]))
